cd35f9929806ae74d4ffad5f7d11c5de37a3dac8
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2024-08-15 14:37:18 +0200

    Add query sorting and numeric filtering

--- controllers/products.js
+++ controllers/products.js
@@ -15,7 +15,7 @@ class ProductControllerClass {
     }
     
     getAllProducts = async (req, res) => {
-        const {featured, company, name, sort, fields} = req.query;
+        const {featured, company, name, sort, fields, numericFilters} = req.query;
         const queryObject = {};
 
         if (featured) {
@@ -33,7 +33,31 @@ class ProductControllerClass {
             };
         }
 
+        if (numericFilters) {
+            const operatorMap = {
+                '>': '$gt',
+                '<' : '$lt',
+                '>=': '$gte',
+                '<=': '$lte',
+                '=': '$eq'
+            }
+
+            const regEx = /\b(<|>|=|<=|>=)\b/g
+            let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+
+            const options = ['price', 'rating'];   
+            filters = filters.split(',').forEach((item) => {
+                const [field,operator,value] = item.split('-')
+                if(options.includes(field)){
+                    queryObject[field] = {
+                        [operator]: Number(value)
+                    }
+                }
+            })
+        }
+
         let result = this.Product.find(queryObject);
+
         if (sort) {
             const sortList = sort.split(',').join(' ');
             result = result.sort(sortList);
@@ -45,6 +69,12 @@ class ProductControllerClass {
             const fieldsList = fields.split(',').join(' ');
             result = result.select(fieldsList)
         }
+
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page-1)*limit;
+
+        result = result.skip(skip).limit(limit);
 
         const product = await result;
         return res.status(200).json({success: true, products: product, nbHits: product.length})
